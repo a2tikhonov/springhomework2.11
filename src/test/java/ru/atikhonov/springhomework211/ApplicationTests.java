2bac1e8591ed4cbb6ff024fb88ff9ac1b7a2142d
a2tikhonov <git!St@d1um@rc@d1um>
2022-07-04 09:01:19 +0300

    Initial commit

---
+++ src/test/java/ru/atikhonov/springhomework211/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.atikhonov.springhomework211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
